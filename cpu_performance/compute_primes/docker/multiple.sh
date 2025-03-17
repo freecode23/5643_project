fa9f60c7b642b69e71e5337357886ce19ac35e70
Sherly Hartono <hartono.s@northeastern.edu>
2025-03-17 17:31:08 -0400

    docker compose works need to kill"

--- cpu_performance/compute_primes/docker/multiple.sh
+++ cpu_performance/compute_primes/docker/multiple.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
 
 SYSBENCH_SCRIPT="sysbench_single.sh"
-INSTANCE_COUNTS=(1 2)
+INSTANCE_COUNTS=(1 2 4)
 
 # 1. Copy sysbench_single.sh from parent directory to current directory
 cp ../${SYSBENCH_SCRIPT} .
 
-# -----------------------------------------
 # 2. Run sysbench with X number of docker instances and log the execution time for each of the instance.
-# -----------------------------------------
-# TODO: for each instance count in instance
 # Loop through each instance count
 for INSTANCE_COUNT_ARG in "${INSTANCE_COUNTS[@]}"; do
-    
-    # Run docker-compose up with the scale option
     INSTANCE_COUNT=${INSTANCE_COUNT_ARG} docker-compose up --build --scale sysbench=${INSTANCE_COUNT_ARG}
-    
-    
 done
 
 # 3. Delete sysbench_single.sh in the current directory.
